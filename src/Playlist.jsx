708705081ae1402ec95be5f0cc901e3c64006200
VikashAnandJha <mailtovikashjha@gmail.com>
2024-09-08 03:30:10 +0530

    new songs

--- src/Playlist.jsx
+++ src/Playlist.jsx
@@ -15,6 +15,18 @@ function Playlist({ changeSong }) {
       name: "Admiring you",
       url: "https://pagalworld.cool/files/download/id/315",
     },
+    {
+      name: "jee ni krda",
+      url: "https://p320.djpunjab.is/data/48/54172/302537/Jee%20Ni%20Lagda%20-%20Karan%20Aujla.mp3",
+    },
+    {
+      name: "Assi gabru punjabi",
+      url: "https://music.djpunjab.com.se/upload_file/414/415/176/400/407/Asi_Gabru_Punjabi_Amrinder_Gill.mp3",
+    },
+    {
+      name: "bol kaffara",
+      url: "https://pagalfree.com/musics/128-Bol%20Kaffara%20Kya%20Hoga%20-%20Neha%20Kakkar%20128%20Kbps.mp3",
+    },
   ];
 
   useEffect(() => {}, []);
